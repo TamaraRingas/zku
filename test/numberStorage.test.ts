0fb2fed578debe1fd1b3ac0dfbd181067d817a11
tamararingas <tamriasdev@gmail.com>
2022-04-17 19:24:03 +0200

    Added test case for retrieval function

--- test/numberStorage.test.ts
+++ test/numberStorage.test.ts
@@ -4,32 +4,27 @@ const {ethers} = require("hardhat");
 const { expect } = require("chai");
 
 describe.only("Storage tests", () => {
-  let owner: SignerWithAddress;
   let alice: SignerWithAddress;
   let StorageArtifacts;
   let store: Contract;
 
   beforeEach(async () => {
-    [owner, alice] = await ethers.getSigners();
+    alice = await ethers.getSigners();
     StorageArtifacts = await ethers.getContractFactory("NumberStorage");
     store = await StorageArtifacts.deploy();
 
-    await barn
-      .connect(owner)
-      .mint(owner.address, ethers.utils.parseEther("1000000000"));
-
-    await barn.connect(owner).mint(owner.address, constants.ERC20.tokenSupply);
   });
 
   describe("Try Retrieve Before Storing", async () => {
-    await expect(store.connect(alice).retrieve().to.revertWith("There is no number to retrieve"));
+    await expect(store.connect(alice).retrieveNumber().to.revertWith("There is no number to retrieve"));
   });
 
   describe("It should store number correctly", async () => {
-    
+    await store.connect(alice).storeNumber(4);
+    await expect(store.number).to.equal(4);
   });
 
   describe("It should retrieve number correctly", async () => {
-    
+    await expect(store.connect(alice).retriveNumber().to.be(4));
   });
 });
