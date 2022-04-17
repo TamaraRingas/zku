272e732856bdbc2a8505ded7d2b94cd1b9ed0f8c
tamararingas <tamriasdev@gmail.com>
2022-04-17 19:58:01 +0200

    Fixed typo

--- contracts/NumberStorage.sol
+++ contracts/NumberStorage.sol
@@ -7,15 +7,18 @@ contract NumberStorage {
   uint256 number;
   bool stored;
 
+  // Sets number to argument passed in
   function storeNumber(uint256 _number) external {
     number = _number;
     stored = true;
   }
 
+  // Returns the number, if one has been stored (the modifier checks this)
   function retrieveNumber() external numberStored returns (uint256) {
     return number;
   }
 
+  // Checks if a number has been stored yet
   modifier numberStored() {
     require(stored, "There is no number to retrieve");
     _;
