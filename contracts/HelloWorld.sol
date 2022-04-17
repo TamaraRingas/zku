a44e14f169a141587e78dad43174d7c5e5bd7c92
tamararingas <tamriasdev@gmail.com>
2022-04-17 19:06:08 +0200

    Added nunmberStored modifier

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -5,12 +5,19 @@ pragma solidity 0.8.13;
 contract HelloWorld {
 
   uint256 number;
+  bool stored;
 
   function storeNumber(uint256 _number) external {
     number = _number;
+    stored = true;
   }
 
-  function retrieveNumber() external returns (bool) {
+  function retrieveNumber() external numberStored returns (bool) {
     return number;
+  }
+
+  modifier numberStored() {
+    require(stored, "There is no number to retrieve");
+    _;
   }
 }
