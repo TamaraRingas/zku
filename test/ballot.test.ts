439758840966fd7c99f73bba05223b3cfef669bf
tamararingas <tamriasdev@gmail.com>
2022-04-18 15:24:42 +0200

    Declared variables

--- test/ballot.test.ts
+++ test/ballot.test.ts
@@ -4,5 +4,11 @@ const {ethers} = require("hardhat");
 const { expect } = require("chai");
 
 describe("Ballot Tests",  () => {
+  let alice: SignerWithAddress;
+  let BallotArtifacts;
+  let ballot: Contract;
   
+  beforeEach() {
+
+  }
 });
