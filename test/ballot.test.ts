01faa7e061d412437a2019b65fe0749bbed7ae65
tamararingas <tamriasdev@gmail.com>
2022-04-18 12:01:35 +0200

    Added import statements

---
+++ test/ballot.test.ts
@@ -0,0 +1,5 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { Contract} from 'ethers';
+const {ethers} = require("hardhat"); 
+const { expect } = require("chai");
+
